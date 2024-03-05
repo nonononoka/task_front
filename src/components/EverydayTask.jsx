da86270205458da7d254620b744ee36ac37e0726
nonononoka <onon.kaori@gmail.com>
2024-03-05 13:25:49 +0900

    add limit

--- src/components/EverydayTask.jsx
+++ src/components/EverydayTask.jsx
@@ -1,63 +1,7 @@
-import { useState } from "react";
-import { gql, useMutation, useQuery } from "@apollo/client";
-
-const EverydayTask = () => {
-  const [val, setVal] = useState("");
-  const clearVal = () => setVal("");
-
-  const ADD_TASK_MUTATION = gql`
-    mutation Mutation($input: String!) {
-      addTask(input: $input) {
-        id
-        name
-        postedBy
-      }
-    }
-  `;
-
-  const ALL_TASKS = gql`
-    query AllTasks {
-      allTasks {
-        id
-        name
-        postedBy
-      }
-    }
-  `;
-
-  const { loading, data,error } = useQuery(ALL_TASKS);
-  
-  const [addTask] = useMutation(ADD_TASK_MUTATION, {
-    refetchQueries: [{ query: ALL_TASKS }],
-  });
-  if(error){
-    console.log(error.message)
-  }
-
-  if (loading || error) return <></>;
-
+const EverydayTask= () => {
   return (
     <>
-      <h1>everyday task</h1>
-      <input
-        placeholder="your task"
-        value={val}
-        onChange={(e) => setVal(e.target.value)}
-      />
-
-      <button onClick={clearVal}>clear</button>
-      <button
-        onClick={() =>
-          addTask({
-            variables: {
-              input: val,
-            },
-          })
-        }
-      >
-        add Task
-      </button>
-      <div>{data.allTasks.map((task) => <p key = {task.id}>{task.name}</p>)}</div>
+      <h1>Everyday Task</h1>
     </>
   );
 };
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,98 @@
+import { useState } from "react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 const TaskList = () => {
+  const [val, setVal] = useState("");
+  const clearVal = () => setVal("");
+
+  const ADD_TASK_MUTATION = gql`
+    mutation Mutation($input: AddTaskInput!) {
+      addTask(input: $input) {
+        name
+        id
+        limitDate
+      }
+    }
+  `;
+
+  const REMOVE_ALL_TASKS_MUTATION = gql`
+    mutation Mutation {
+      removeAllTasks
+    }
+  `;
+
+  const ALL_TASKS = gql`
+    query AllTasks {
+      allTasks {
+        id
+        name
+        postedBy
+        limitDate
+      }
+    }
+  `;
+
+  const { loading, data, error } = useQuery(ALL_TASKS);
+  const Today = new Date();
+  const [date, setDate] = useState(Today);
+  console.log(date);
+
+  const [addTask] = useMutation(ADD_TASK_MUTATION, {
+    refetchQueries: [{ query: ALL_TASKS }],
+  });
+  const [removeAllTasks] = useMutation(REMOVE_ALL_TASKS_MUTATION, {
+    refetchQueries: [{ query: ALL_TASKS }],
+  });
+  if (error) {
+    console.log(error.message);
+  }
+
+  if (loading || error) return <></>;
+
   return (
     <>
-      <h1>Task List</h1>
+      <h1>task list</h1>
+      <input
+        placeholder="your task"
+        value={val}
+        onChange={(e) => setVal(e.target.value)}
+      />
+      <div>
+        <DatePicker
+          showIcon
+          selected={date}
+          onChange={(selectedDate) => {
+            setDate(selectedDate || Today);
+          }}
+        />
+      </div>
+      <button onClick={clearVal}>clear</button>
+      <button
+        onClick={() =>
+          addTask({
+            variables: {
+              input: {
+                name: val,
+                limitDate: date.toISOString(),
+              },
+            },
+          })
+        }
+      >
+        add Task
+      </button>
+      <div>
+        {data.allTasks.map((task) => (
+          <>
+            <p key={task.id}>
+              name: {task.name} limit: {task.limitDate.split("T")[0]}
+            </p>
+          </>
+        ))}
+      </div>
+      <button onClick={() => removeAllTasks()}>delete all tasks</button>
     </>
   );
 };
